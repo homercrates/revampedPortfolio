13985e54eead35f229ea5fcee7c2e1382200c0c9
smw mc <smeredithwilliams@gmail.com>
2020-04-01 13:38:00 -0700

    initial commit, boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { Layout } from '../components/Layout'
+
+export const IndexPage = () => (
+  <>
+    <h2>Developer</h2>
+    <p>here is some stuff.</p>
+    <p>here is a little more</p>
+    <ul><li>here</li><li>some</li><li>stuff</li></ul>
+
+    <p>
+      <Link to="/another-page/">Go to another page</Link>
+    </p>
+    <p>
+      <Link to="/all/">See content generated from Markdown files</Link>
+    </p>
+  </>
+)
+
+const LayoutIndexPage = () => (
+  <Layout>
+    <IndexPage />
+  </Layout>
+)
+
+export default LayoutIndexPage
