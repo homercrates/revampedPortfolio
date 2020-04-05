9536b145f3745795d25caa4ea87055523ce1ab85
smw mc <smeredithwilliams@gmail.com>
2020-04-04 23:15:33 -0700

    Installed react-icons, Added github and linked in icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { Layout } from '../components/Layout'
 import { css } from '@emotion/core'
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const style = {
+  grey: css`
+    color: #3e3e3e;
+  `,
   accentColor: css`
     color: #6f42c1;
   `,
@@ -18,7 +22,10 @@ export const IndexPage = () => (
     <p>here is some stuff.</p>
     <p>here is a little more</p>
     <ul><li>here</li><li>some</li><li>stuff</li></ul>
-
+    <h1 css={style.boldLook}>
+      <Link to="/all/" css={style.grey}><FaGithub /></Link>
+      <Link to="/all/" css={style.grey}><FaLinkedin /></Link>
+    </h1>
     <p>
       <Link to="/another-page/">Go to another page</Link>
     </p>
