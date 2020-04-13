2c818f24f244bd586b4fa2a2784d7450848f8f1a
smw mc <smeredithwilliams@gmail.com>
2020-04-13 13:35:52 -0700

    Fixed dripClock page

--- src/pages/dripClock.tsx
+++ src/pages/dripClock.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Layout } from '../components/Layout'
+
 import { css } from '@emotion/core'
 
+const some = css`
+  color: purple;
+  background-color: black;
+`
 
-const DripClock = () => (
-  <p>Drip Clock</p>
+export const DripClock = () => (
+  <div>
+    <p css={some}>Drip Clock</p>
+    <p>heu</p>
+    <p>more things</p>
+    {console.log('loadede drip clock')}
+    {alert('jry')}
+  </div>
 )
-export default DripClock;
+
+const LayoutDripClock = () => (
+    <Layout>
+        <DripClock />
+        {console.log('loaded drip clock')}
+    </Layout>
+)
+export default LayoutDripClock;
